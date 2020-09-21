2377e20fd289a58cd8f6eac9503b1d8c2ebf32d5
Nelson Foltz <nelsfoltz@gmail.com>
2020-09-21 10:46:34 -0400

    add: useLocalStorage hook

--- src/hooks/useTodoState.js
+++ src/hooks/useTodoState.js
@@ -1,15 +1,18 @@
 import { useState } from 'react';
+import { useLocalStorageState } from './useLocalStorageState';
 import { v4 as uuid } from 'uuid';
 
 export default initialTodos => {
-  const [todos, setTodos] = useState(initialTodos);
+  const [todos, setTodos] = useLocalStorageState('todos', initialTodos);
   return {
     todos,
     addTodo: newTodoText => {
       setTodos([...todos, { id: uuid(), task: newTodoText, completed: false }]);
     },
     removeTodo: todoId => {
+      //filter out removed todo
       const updatedTodos = todos.filter(todo => todo.id !== todoId);
+      //call setTodos with new todos array
       setTodos(updatedTodos);
     },
     toggleTodo: todoId => {
